6901e7f2c44aa5403c9a1c4f540046e3bb6eb5c7
전소희 <jeonsohui@jeonsohuis-MacBook-Pro.local>
2023-06-03 04:11:03 +0900

    Learn:Thumbnail

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -11,11 +11,7 @@ import { protectorMiddleware, videoUpload } from "../middlewares";
 
 const videoRouter = express.Router();
 
-videoRouter
-  .route("/upload")
-  .all(protectorMiddleware)
-  .get(getUpload)
-  .post(videoUpload.single("video"), postUpload);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter
   .route("/:id([0-9a-f]{24})/edit")
   .all(protectorMiddleware)
@@ -25,6 +21,16 @@ videoRouter
   .route("/:id([0-9a-f]{24})/delete")
   .all(protectorMiddleware)
   .get(deleteVideo);
-videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(
+    videoUpload.fields([
+      { name: "video", maxCount: 1 },
+      { name: "thumb", maxCount: 1 },
+    ]),
+    postUpload
+  );
 
 export default videoRouter;
